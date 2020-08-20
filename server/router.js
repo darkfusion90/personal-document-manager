6a864dd4996285dfa4085addb1e702a24cf1600e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-20 12:25:32 +0530

    Added more routes to router

--- server/router.js
+++ server/router.js
@@ -1,8 +1,26 @@
 const api = require('./api')
+const { upload } = require('./upload')
+
+const documentsRouter = expressApp => {
+    const documents = api.documents
+
+    expressApp.get('/api/documents/:id', documents.get)
+    expressApp.post('/api/documents', documents.post)
+    expressApp.put('/api/documents/:id', upload.single('document'), documents.put.uploadDocument)
+}
+
+const usersRouter = expressApp => {
+    expressApp.get('/api/users/:id?', api.users.get)
+}
+
+const filesRouter = expressApp => {
+    expressApp.get('/api/files/:id', api.files.get)
+}
 
 const router = expressApp => {
-    expressApp.get('/api/users/:id?', api.users.get)
-    expressApp.get('/api/documents/:id', api.documents.get)
+    usersRouter(expressApp)
+    documentsRouter(expressApp)
+    filesRouter(expressApp)
 }
 
 module.exports = router
