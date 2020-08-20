abdbcf4a6dce0701a3c080764893b6c2b5cd1b77
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-20 12:24:51 +0530

    Wait for database connection before listening as server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-require('./database').connect()
-const expressApp = require('./app')
+require('./database').connect().then(setupServer)
 
-expressApp.listen(8000, () => console.log('Server listening atr port: 8000'))
+function setupServer() {
+    const expressApp = require('./app')
+    expressApp.listen(8000, () => console.log('Server listening atr port: 8000'))
+}
+
+
