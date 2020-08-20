ebedbbf046c079d8a315e6ae1fd7d55c03774113
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-20 12:27:35 +0530

    Create individual document separately to avoid unnecessary null field

--- server/database/controllers/documents/create.js
+++ server/database/controllers/documents/create.js
@@ -1,14 +1,22 @@
 const { DocumentModel } = require('../../models/DocumentModel')
 const wrapCallback = require('../../../utils/wrapCallback')
 
+const saveDocument = (document, callback) =>
+    wrapCallback((resolve, reject) => {
+        document.save().then(resolve, reject)
+    }, callback)
+
+
 const createDocument = (name, user, docGroupId, callback) => {
     const document = new DocumentModel({ name, user, groupId: docGroupId })
 
-    return wrapCallback((resolve, reject) => {
-        document.save().then(resolve, reject)
-    }, callback)
+    return saveDocument(document, callback)
 }
 
-const createIndividualDocument = (name, user, callback) => createDocument(name, user, null, callback)
+const createIndividualDocument = (name, user, callback) => {
+    const document = new DocumentModel({ name, user })
+
+    return saveDocument(document, callback)
+}
 
 module.exports = { createIndividualDocument, createGroupDocument: createDocument }
