3c4c9cc8488ba0921ae178a6ae24996548fcc198
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-20 12:26:44 +0530

    Added function to connect and wait on the promise returned

--- server/database/index.js
+++ server/database/index.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true)
 mongoose.set('useFindAndModify', false)
 
+mongoose.connection.on('open', function cb() {
+    console.log("connected to db")
+})
+mongoose.connection.on('error', function cb(err) {
+    console.log("Error connecting to db: ", err)
+})
+
 module.exports = {
     connect: function () {
         console.log("Mongodb Connection URI: ", config.MONGO_URI)
-        mongoose.connect(config.MONGO_URI, config.MONGO_CLIENT_OPTS);
-        mongoose.connection.on('open', function cb() {
-            console.log("connected to db")
-        })
-        mongoose.connection.on('error', function cb(err) {
-            console.log("Error connecting to db: ", err)
-        })
+        return mongoose.connect(config.MONGO_URI, config.MONGO_CLIENT_OPTS);
     }
 }
 
