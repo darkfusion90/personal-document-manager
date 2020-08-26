43dc776fb82c3152d5018d148311a6e6ee35826d
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-26 21:30:18 +0530

    Added method to get file's download url

--- client/src/api/files.ts
+++ client/src/api/files.ts
@@ -1,5 +1,18 @@
+import DocumentFileModel, {
+    fromJson as jsonToDocumentFile
+} from "../store/models/DocumentFileModel"
+import axios from './config'
+
 const kFileHomeUrl: string = '/api/files'
 
 const getFileUrl = (fileId: string): string => `${kFileHomeUrl}/${fileId}`
 
-export default { getFileUrl }
+const getFileDownloadUrl = (fileId: string): string => `${getFileUrl(fileId)}/download`
+
+const getFile = async (fileId: string): Promise<DocumentFileModel> => {
+    const response = await axios.get(getFileUrl(fileId))
+
+    return jsonToDocumentFile(response.data)
+}
+
+export default { getFile, getFileUrl, getFileDownloadUrl }
