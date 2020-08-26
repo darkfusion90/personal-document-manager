0b5fa67ea6d2212b6d0a459e9d68dadd928fea45
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-26 21:29:43 +0530

    Added global styling to make cursor disabled for disabled btns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import DocumentGrid from './components/DocumentGrid'
 
+import './App.css'
+
 const App = () => {
     return (
-        <DocumentGrid />
+        <div className='m-5'>
+            <DocumentGrid />
+        </div>
     )
 }
 
