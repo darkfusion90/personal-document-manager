a7b122239e00cc647ff1cad3ffcbd5678583a973
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-26 21:29:08 +0530

    Added more type checking. Added api to upload file for document

--- client/src/api/documents.ts
+++ client/src/api/documents.ts
@@ -1,6 +1,6 @@
-import api from './config'
+import axios from './config'
 
-import DocumentModel from '../store/models/DocumentModel'
+import DocumentModel, { fromJson as docFromJson } from '../store/models/DocumentModel'
 
 const kDocumentHomeUrl: string = '/api/documents'
 
@@ -8,20 +8,29 @@ const getDocumentUrl = (docId: string): string => `${kDocumentHomeUrl}/${docId}`
 
 const getDocument = (docId: string): Promise<any> => {
     const url: string = getDocumentUrl(docId)
-    return api.get(url)
+    return axios.get(url)
 }
 
 const getAllDocuments = async (): Promise<Array<DocumentModel>> => {
-    const response = await api.get(kDocumentHomeUrl)
+    const response = await axios.get(kDocumentHomeUrl)
     const documentMeta: ArrayResponseData<DocumentModel> = response.data
+    const docsListJson = documentMeta.data
 
-    return documentMeta.data
+    return docsListJson.map(docFromJson)
 }
 
 const createDocument = async (name: string): Promise<DocumentModel> => {
-    const response = await api.post(kDocumentHomeUrl, { name })
+    const response = await axios.post(kDocumentHomeUrl, { name })
 
-    return response.data
+    return docFromJson(response.data)
 }
 
-export default { getDocument, getAllDocuments, createDocument }
+const uploadDocumentFile = async (id: string, file: File): Promise<DocumentModel> => {
+    const document: FormData = new FormData()
+    document.append('document', file)
+
+    const response = await axios.put(getDocumentUrl(id), document)
+    return docFromJson(response.data)
+}
+
+export default { getDocument, getAllDocuments, createDocument, uploadDocumentFile }
