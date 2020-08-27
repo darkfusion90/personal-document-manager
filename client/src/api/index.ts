00e02e3bb6f6f960f7d476e0f4173eb16032cfad
darkfusion90 <gauravsharmaa341@gmail.com>
2020-08-27 19:21:37 +0530

    Fixed imports

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,9 @@
 import documents from './documents'
 import files from './files'
+import account from './account'
 
 export { files }
 export { documents }
+export { account }
 
-export default { documents, files }
+export default { documents, files, account }
